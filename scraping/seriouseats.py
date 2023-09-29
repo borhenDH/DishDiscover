5cd5862a1800e22d17dfd1b24296252e470fc5fd
borhen eddine dahech <bdahech08@gmail.com>
2023-09-29 09:51:31 +0100

    Update seriouseats.py

--- scraping/seriouseats.py
+++ scraping/seriouseats.py
@@ -8,10 +8,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 import time
 import psycopg2
-import redis
 import json
 import re
 import boto3
 from unidecode import unidecode
 from dotenv import load_dotenv
 import os
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+service = Service(PATH)
+driver = webdriver.Chrome(service=service)
+driver.get("https://www.allrecipes.com/ingredients-a-z-6740416")   
+
+links=[]
+try:    
+    container = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID,'alphabetical-list_1-0')))
+    alphabets = container.find_elements(By.CLASS_NAME,'alphabetical-list__group')
+except:
+    print('error in getting links')
+
+for k,alphabet in enumerate(alphabets) :
+    link = alphabet.find_element(By.TAG_NAME, 'a').get_attribute("href")
