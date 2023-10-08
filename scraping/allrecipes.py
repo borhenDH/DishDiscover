8174b3b5a86e3f850f54de21d5807b169aab92d6
borhen eddine dahech <bdahech08@gmail.com>
2023-10-08 14:14:54 +0100

    Update allrecipes.py

--- scraping/allrecipes.py
+++ scraping/allrecipes.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
@@ -13,38 +13,31 @@ import re
 from unidecode import unidecode
 from dotenv import load_dotenv
 import os
-from selenium import webdriver
 
-
-from selenium import webdriver
-
-from selenium.webdriver.chrome.service import Service
-
-service = Service() # this is the important line of code!
 
 PATH = r"C:\Program Files (x86)\msedgedriver"
 service = Service(PATH)
-
 driver = webdriver.Edge(service=service)
 
 
-
 driver.get("https://www.allrecipes.com/ingredients-a-z-6740416")
-
+time.sleep(5)
 links=[]
 try:    
     container = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID,'alphabetical-list_1-0')))
-    time.sleep(5)
+    
     alphabets = container.find_elements(By.CLASS_NAME,'alphabetical-list__group')
+    print(alphabets)
 except:
     print('error in getting links')
 
 for k,alphabet in enumerate(alphabets) :
-    ingredients = alphabet.find_elements(By.CLASS_NAME, 'link-list__item')
-    time.sleep(5)
-    for i,ingredient in enumerate(ingredients):
-        link = ingredient.get_attribute("href")
-        if i>3 :
-            break
+    list_of_ingredients = alphabet.find_element(By.TAG_NAME, 'ul')
+    ingredient=list_of_ingredients.find_elements(By.TAG_NAME,"li")
+    for i,element in enumerate(ingredient):
+        
+        link_container = element.find_element(By.TAG_NAME,"a")
+        link = link_container.get_attribute("href")
         links.append(link)
-print(links)
+
+
